0ae0f771a0efbf60617ae37db7a972f553d9b339
pablinesamara <pablinesamara@gmail.com>
2024-05-27 13:22:36 -0300

    first commit

---
+++ src/MinhaClasse.java
@@ -0,0 +1,6 @@
+public class MinhaClasse {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Programa executado pelo terminal :-)");
+        System.out.println("Teste 2");
+    }
+}
